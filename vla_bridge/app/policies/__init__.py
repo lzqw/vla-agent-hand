388d703f82919abf5b8c796d420463b6d802a82f
lzqw <lizeqiaoemail@163.com>
2026-08-27 23:07:01 +0800

    Register pure joint VLA policies

--- vla_bridge/app/policies/__init__.py
+++ vla_bridge/app/policies/__init__.py
@@ -8,14 +8,18 @@ from .base import (
     PolicyResult,
     UnavailablePolicy,
 )
+from .bc_joint_policy import BCJointVLAPolicy
 from .expert_lookup_policy import ExpertLookupPolicy
+from .joint_vla_policy import JointVLAPolicy
 from .rabo_vla_policy import RaboVLAPolicy
 from .zero_policy import ZeroPolicy
 
 __all__ = [
     "BasePolicy",
+    "BCJointVLAPolicy",
     "ExpertLookupPolicy",
     "ImageInput",
+    "JointVLAPolicy",
     "PolicyError",
     "PolicyInputError",
     "PolicyNotReadyError",
