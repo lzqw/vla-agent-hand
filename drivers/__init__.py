40598f3dd10e6a03cd4cf01eacfd586ddf184a67
lzqw <lizeqiaoemail@163.com>
2026-08-27 22:44:36 +0800

    Export VLA action adapter

--- drivers/__init__.py
+++ drivers/__init__.py
@@ -4,6 +4,7 @@ from .rabo_config import CollectorConfig, load_config
 from .rabo_devices import RaboDevices
 from .rabo_remote_command import RemoteCommandExecutor
 from .rabo_sensors import MockSensorBackend, Ros2SensorBackend
+from .vla_action_adapter import VLA_ACTION_SPACE, VLAActionAdapter
 
 __all__ = [
     "CollectorConfig",
@@ -11,5 +12,7 @@ __all__ = [
     "RaboDevices",
     "RemoteCommandExecutor",
     "Ros2SensorBackend",
+    "VLA_ACTION_SPACE",
+    "VLAActionAdapter",
     "load_config",
 ]
